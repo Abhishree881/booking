3938569c9ba24f98326a2bef1458c6dd8e7384e7
Abhishree881 <abhishree881@gmail.com>
2024-12-30 01:20:40 +0530

    changed stytch env to live

--- src/lib/stytchClient.js
+++ src/lib/stytchClient.js
@@ -4,5 +4,5 @@ import * as stytch from 'stytch';
 export const stytchClient = new Client({
   project_id: "project-test-e0090fcb-3bc7-44e4-8112-7f6978fa682b",
   secret: "secret-test-YT-zdL1pXZ8TTN4Ijx4rOtrc79QN4mdPcFc=",
-  env: stytch.envs.test, 
+  env: stytch.envs.live, 
 });
