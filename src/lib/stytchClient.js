532189b2b084a6e5c16e73713b09d26a83ab6c1a
Abhishree881 <abhishree881@gmail.com>
2024-12-30 01:39:28 +0530

    changed env ids to live

--- src/lib/stytchClient.js
+++ src/lib/stytchClient.js
@@ -2,7 +2,7 @@ import { Client } from 'stytch';
 import * as stytch from 'stytch';
 
 export const stytchClient = new Client({
-  project_id: "project-test-e0090fcb-3bc7-44e4-8112-7f6978fa682b",
-  secret: "secret-test-YT-zdL1pXZ8TTN4Ijx4rOtrc79QN4mdPcFc=",
+  project_id: "project-live-7f3d5b7a-ca6f-4e5c-a5df-0ff2889dbaa8",
+  secret: "secret-live-GBMm_yeUiQ0y9jwmYAhCUqDu62rOgzrZ-Mw=",
   env: stytch.envs.live, 
 });
