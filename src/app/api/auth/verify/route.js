c475359ad0610120297cd0d7e79aa8062b80d83e
Abhishree881 <abhishree881@gmail.com>
2024-12-30 01:10:38 +0530

    added users to database

--- src/app/api/auth/verify/route.js
+++ src/app/api/auth/verify/route.js
@@ -2,15 +2,25 @@ import { NextResponse } from 'next/server';
 import { stytchClient } from '@/lib/stytchClient';
 import { getIronSession } from 'iron-session/edge';
 import { sessionOptions } from '@/lib/sessionOptions';
+import { supabaseClient } from '@/lib/supabaseClient';
 
 export async function POST(req) {
   try {
     const { token } = await req.json();
 
-    const response = await stytchClient.magicLinks.authenticate( {
-        token,
-        session_duration_minutes: 60, 
+    const response = await stytchClient.magicLinks.authenticate({
+      token,
+      session_duration_minutes: 60,
     });
+
+    const id = response.user.user_id;
+    const email = response.user.emails[0].email;
+    const name = response.user.name.first_name;
+    const { data, error } = await supabaseClient
+      .from("users")
+      .upsert({ id, email, name }, { onConflict: "id" });
+
+    if (error) throw error;
 
     const res = NextResponse.json({ message: 'Token verified successfully', session: response.user });
 
