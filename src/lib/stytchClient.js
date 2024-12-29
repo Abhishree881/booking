1a387128cbd27962befca07ba5a9626053fcd574
Abhishree881 <abhishree881@gmail.com>
2024-12-30 01:47:17 +0530

    removed api keys from code

--- src/lib/stytchClient.js
+++ src/lib/stytchClient.js
@@ -2,7 +2,7 @@ import { Client } from 'stytch';
 import * as stytch from 'stytch';
 
 export const stytchClient = new Client({
-  project_id: "project-live-7f3d5b7a-ca6f-4e5c-a5df-0ff2889dbaa8",
-  secret: "secret-live-GBMm_yeUiQ0y9jwmYAhCUqDu62rOgzrZ-Mw=",
-  env: stytch.envs.live, 
+  project_id: process.env.STYTCH_PROJECT_ID,
+  secret: process.env.STYTCH_SECRET,
+  env: process.env.STYTCH_ENV === "test" ? stytch.envs.test : stytch.envs.live, 
 });
