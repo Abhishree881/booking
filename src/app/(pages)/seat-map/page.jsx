5a412d8d3959d161a5eda440f750af6184bd1731
Abhishree881 <abhishree881@gmail.com>
2024-12-30 14:39:07 +0530

    completed seat map page

--- src/app/(pages)/seat-map/page.jsx
+++ src/app/(pages)/seat-map/page.jsx
@@ -5,37 +5,54 @@ import React, { useEffect, useState } from 'react'
 import "@/styles/seatmap.css"
 import ChairIcon from '@mui/icons-material/Chair';
 import ChairOutlinedIcon from '@mui/icons-material/ChairOutlined';
+import { Button, Tag } from 'antd';
+import toast from 'react-hot-toast';
 
 const page = () => {
   const [loading, setLoading] = useState(true)
-  const [seats, setSeats] = useState([])
   const [rows, setRowSeats] = useState([])
   const [availableSeats, setAvailableSeats] = useState([])
 
-  useEffect(() => {
-    const fetchApiData = async () => {
-      setLoading(true)
-      try {
-        const response = await fetch("/api/seats")
-        const data = await response.json()
-        setSeats(data.availableSeats)
-        data.availableSeats.forEach((seat) => {
-          setAvailableSeats((prevSeats) => [...prevSeats, seat.seat_number])
-        })
-        setLoading(false)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    const numbers = Array.from({ length: 80 }, (_, i) => i + 1); // Generate numbers from 1 to 80
-    const rows = [];
+  const fetchApiData = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch("/api/seats")
+      const data = await response.json()
+      const availableSeats = []
+      data.availableSeats.forEach((seat) => {
+        availableSeats.push(seat.seat_number)
+      })
+      setAvailableSeats(availableSeats.sort((a, b) => a - b))
+      setLoading(false)
+      const numbers = Array.from({ length: 80 }, (_, i) => i + 1); // Generate numbers from 1 to 80
+      const rows = [];
 
-    for (let i = 0; i < numbers.length; i += 7) {
-      rows.push(numbers.slice(i, i + 7));
+      for (let i = 0; i < numbers.length; i += 7) {
+        rows.push(numbers.slice(i, i + 7));
+      }
+      setRowSeats(rows)
+    } catch (error) {
+      toast.error(error)
     }
-    setRowSeats(rows)
+  }
+
+  useEffect(() => {
     fetchApiData()
   }, [])
+
+  const handleReset = async () => {
+    try {
+      const response = await fetch("/api/tickets/reset")
+      if (response.ok) {
+        toast.success("Seats Reset Successfully")
+      } else {
+        toast.error("Something went wrong")
+      }
+      fetchApiData()
+    } catch (error) {
+      toast.error(error)
+    }
+  }
 
 
   return (
@@ -44,6 +61,12 @@ const page = () => {
         <div className='seat-map'>
           <div className='seat-map__left'>
             <h1>Available Seat Map</h1>
+            <div className='seat-map__left__seats'>
+              <Tag color="blue" style={{ margin: '4px' }}>Available: {availableSeats.length}</Tag>
+              <Tag color="blue" style={{ margin: '4px' }}>Booked: {80 - availableSeats.length}</Tag>
+              <Tag color="blue" style={{ margin: '4px' }}>Total: {80}</Tag>
+              <Button onClick={handleReset} color="blue" style={{ margin: '4px' }} variant='solid' size='small'>Reset</Button>
+            </div>
             {rows.map((row, rowIndex) => (
               <div key={rowIndex} className='seat-row'>
                 {row.map((seatNumber) => (
@@ -62,10 +85,10 @@ const page = () => {
           <div className='seat-map__right'>
             <h1>Available Seat Chart</h1>
             <div className='seat-chart'>
-              {seats.map((seat, index) => (
+              {availableSeats.map((seat, index) => (
                 <div key={index} className='seat'>
                   <div>Seat </div>
-                  <div className='seat__number'>{seat.seat_number}</div>
+                  <div className='seat__number'>{seat}</div>
                 </div>
               ))}
             </div>
